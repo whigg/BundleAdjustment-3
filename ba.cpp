839df83d9912e863b688b4aadd824743178ce943
zjucxh <chen.xianghui@hotmail.com>
2019-09-24 21:15:00 +0800

    加入了blaocks数据结构，这样在求J(i,j)的时候能够更高效地查找所需要的数据

--- ba.cpp
+++ ba.cpp
@@ -5,30 +5,7 @@
 
 
 int main(int argc, char **argv) {
-    //Function func_arr[3];
-    std::vector<unsigned> cameraIndex;
-    std::vector<unsigned> pointIndex;
-    cv::Mat observation;
-    cv::Mat camera;
-    cv::Mat points;
-    LoadData("./data/problem-49-7776-pre.txt",
-	     cameraIndex, pointIndex, observation, camera, points);
-    
-    printf("size point index = %lu\n", pointIndex.size());
-    printf("size camera index = %lu\n", cameraIndex.size());
-    printf("size of camera = (%u, %u)\n", camera.rows, camera.cols);
-    printf("size of points = (%u, %u)\n", points.rows, points.cols);
-    printf("size of observation = (%u, %u)\n", observation.rows, observation.cols);
-    double total_cost = TotalCost(reprojection_err, camera, points, cameraIndex, pointIndex,
-				  observation);
-    printf("total cost = %lf\n",total_cost);
-    cv::Mat JacMatrix = Jacobian(reprojection_err, camera, points, cameraIndex, pointIndex,
-				 observation);
-    printf("Jacobian column size = (%d, %d)\n", JacMatrix.rows, JacMatrix.cols);
-    
-    cv::namedWindow("bitmap", cv::WINDOW_NORMAL);
-    cv::imshow("bitmap", JacMatrix);
-    //cv::imwrite("./bitmap.jpg", JacMatrix);
-    cv::waitKey(0);
-    return 0;
+  BAProblem problem("./data/problem-49-7776-pre.txt");
+  problem.print_blocks();
+  return 0;
 }
--- public.hpp
+++ public.hpp
@@ -1,20 +1,88 @@
 #ifndef PUBLIC_HPP
 #define PUBLIC_HPP
 #include <stdio.h>
+#include <vector>
 
-#include <opencv2/opencv.hpp>
-cv::Mat selfMultiply(cv::Mat const & m ) {/// return a' * a
-  //assert(mul.cols == m.rows);
-  cv::Mat result = cv::Mat::zeros(m.cols, m.cols, CV_32F );
-  for(int i=0; i<m.cols; i++) {
-    for(int j = 0; j<m.cols; j++) {
-      for(int k = 0; k<m.rows; k++) {
-	result.at<float>(i,j) +=
-	  m.at<float>(k,i) * m.at<float>(k,j);//m.at<double>(k,i)
-      }
-    }
+struct Point3f{
+  float x, y, z;
+};
+
+struct Camera {
+  float r1, r2, r3, t1, t2, t3, f, k1,k2;
+  
+};
+struct Observation{
+  int cid;
+  int pid;
+  float x;
+  float y;
+};
+struct BAProblem {
+private:
+  int camera_number;
+  int point_number;
+  int observation_number;
+  std::vector<Point3f> point_data;
+  std::vector<Camera> camera_data;
+  std::vector<Observation> observation_data;
+  std::vector<int> blocks;// blocks index for accelerating find projection(i,j)
+public:
+  float perr(int const pid, int const cid ) {//calculate the projection error of point pid projecting to camera(cid) 
+    //TODO reprojection i,j
+    
   }
-  return result;
-}
+  BAProblem(char const * file_name ) {//load data
+    FILE * fp = fopen(file_name, "r");
+    if(!fp) {
+      perror("error opening file\n");
+      exit(-1);
+    }
+    fscanf(fp, "%d", &camera_number);
+    fscanf(fp, "%d", &point_number);
+    fscanf(fp, "%d", &observation_number);
+    camera_data.clear();
+    point_data.clear();
+    observation_data.clear();
+    blocks.clear();
+    int current_pid = 0;
+    int current_pid_num = 0;
+    for(int i=0; i<observation_number; i++) {
+      Observation obs;
+      fscanf(fp, "%d", &(obs.cid));
+      fscanf(fp, "%d", &(obs.pid));
+      fscanf(fp, "%f", &(obs.x));
+      fscanf(fp, "%f", &(obs.y));
+      if(current_pid == obs.pid) {
+        //printf("obs.pid = %d\n", obs.pid);
+	      current_pid_num++;
+      }else {
+	      blocks.push_back(current_pid_num);
+	      current_pid_num = 1;
+        current_pid = obs.pid;
+      }
+      observation_data.push_back(obs);
+    }
+    blocks.push_back(current_pid_num);
+    for(int i=0; i<camera_number; i++) {
+      Camera cam;
+      fscanf(fp, "%f", &(cam.r1));
+      fscanf(fp, "%f", &(cam.r2));
+      fscanf(fp, "%f", &(cam.r3));
+      fscanf(fp, "%f", &(cam.t1));
+      fscanf(fp, "%f", &(cam.t2));
+      fscanf(fp, "%f", &(cam.t3));
+      fscanf(fp, "%f", &(cam.f));
+      fscanf(fp, "%f", &(cam.k1));
+      fscanf(fp, "%f", &(cam.k2));
+    }
+    for(int i=0; i<point_number; i++) {
+      Point3f pt;
+      fscanf(fp, "%f", &(pt.x));
+      fscanf(fp, "%f", &(pt.y));
+      fscanf(fp, "%f", &(pt.z));
+    }
+    fclose(fp);
+  }
+};
 
 #endif
