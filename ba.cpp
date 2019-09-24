b4e0ab6ca08fab21d54dbe2e00e8e1170e5ada04
zjucxh <chen.xianghui@hotmail.com>
2019-09-24 18:25:34 +0800

    sparse jacobian matrix solution

--- ba.cpp
+++ ba.cpp
@@ -11,15 +11,19 @@ int main(int argc, char **argv) {
     cv::Mat observation;
     cv::Mat camera;
     cv::Mat points;
-    LoadData("./data/problem-49-7776-pre.txt", cameraIndex, pointIndex, observation, camera, points);
+    LoadData("./data/problem-49-7776-pre.txt",
+	     cameraIndex, pointIndex, observation, camera, points);
+    
     printf("size point index = %lu\n", pointIndex.size());
     printf("size camera index = %lu\n", cameraIndex.size());
     printf("size of camera = (%u, %u)\n", camera.rows, camera.cols);
     printf("size of points = (%u, %u)\n", points.rows, points.cols);
     printf("size of observation = (%u, %u)\n", observation.rows, observation.cols);
-    double total_cost = TotalCost(reprojection_err, camera, points, cameraIndex, pointIndex, observation);
+    double total_cost = TotalCost(reprojection_err, camera, points, cameraIndex, pointIndex,
+				  observation);
     printf("total cost = %lf\n",total_cost);
-    cv::Mat JacMatrix = Jacobian(reprojection_err, camera, points, cameraIndex, pointIndex, observation);
+    cv::Mat JacMatrix = Jacobian(reprojection_err, camera, points, cameraIndex, pointIndex,
+				 observation);
     printf("Jacobian column size = (%d, %d)\n", JacMatrix.rows, JacMatrix.cols);
     
     cv::namedWindow("bitmap", cv::WINDOW_NORMAL);
--- public.hpp
+++ public.hpp
@@ -4,16 +4,17 @@
 
 #include <opencv2/opencv.hpp>
 cv::Mat selfMultiply(cv::Mat const & m ) {/// return a' * a
-    //assert(mul.cols == m.rows);
-    cv::Mat result = cv::Mat::zeros(m.cols, m.cols, CV_32F );
-    printf("...\n");
-    for(int i=0; i<m.cols; i++) {
-        for(int j = 0; j<m.cols; j++) {
-            for(int k = 0; k<m.rows; k++) {
-                result.at<float>(i,j) += m.at<float>(k,i) * m.at<float>(k,j);//m.at<double>(k,i)
-            }
-        }
+  //assert(mul.cols == m.rows);
+  cv::Mat result = cv::Mat::zeros(m.cols, m.cols, CV_32F );
+  for(int i=0; i<m.cols; i++) {
+    for(int j = 0; j<m.cols; j++) {
+      for(int k = 0; k<m.rows; k++) {
+	result.at<float>(i,j) +=
+	  m.at<float>(k,i) * m.at<float>(k,j);//m.at<double>(k,i)
+      }
     }
-    return result;
+  }
+  return result;
 }
+
 #endif
